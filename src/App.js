a0d56ccfb5a83ec6496b83c04f163af5807c5f9b
maaaaaaaaad <integral5137@gmail.com>
2022-03-23 15:44:19 +0900

    add config environment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    console.log(
+      `React start environment server ${process.env.REACT_APP_SERVER_URI}`
+    );
+  });
+
   return <section className="text-4xl text-cyan-600">Hello Wold</section>;
 };
 
