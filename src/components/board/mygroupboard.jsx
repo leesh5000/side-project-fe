e1fdcfe17d655773a9dffcc3bd2b56f5dc4c56cc
dianestar <dianestar@naver.com>
2022-03-24 18:28:08 +0900

    markup MyGroup page

---
+++ src/components/board/mygroupboard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MyGroupBoard = () => {
+    return (
+      <section className="border-b-2 border-slate-400 p-4 w-full mx-auto">
+        <article className="flex space-x-4 items-center">
+            <section className="rounded-lg bg-slate-700 h-24 w-24"></section>
+            <section className="flex-1 space-y-2 py-1">
+                <article className="flex space-x-2 items-center">
+                    <section className="rounded-full border-2 border-slate-400 w-6 h-6 text-slate-400 flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                        </svg>
+                    </section>
+                    <section className="text-sm text-slate-400">활동지역</section>
+                </article>
+                <article className="text-lg font-semibold">모임명모임제목</article>
+                <article className="text-xs text-slate-400 text-right">인원</article>
+            </section>
+        </article>
+      </section>
+    );
+};
+
+export default MyGroupBoard;
--- src/pages/root/MyGroup.jsx
+++ src/pages/root/MyGroup.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import BoardSkeleton from "../../components/skeleton/BoardSkeleton";
+import PlusButton from "../../components/button/PlusButton";
+import MyGroupBoard from "../../components/board/mygroupboard";
+
+const skeletonCount = Array(15).fill(<BoardSkeleton />);
+const recommendGroupsCount = Array(15).fill(<MyGroupBoard />);
 
 const MyGroup = () => {
-  return <section>my group</section>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [joinedGroups, setJoinedGroups] = useState([]);
+
+  return (
+    <section className="w-full">
+      <Helmet>
+        <title>저모이모 | 내모임</title>
+      </Helmet>
+      <article>
+        <section
+          className="
+            text-indigo-500 text-base flex basis-full items-center mt-4
+            
+            before:grow
+            before:bg-indigo-500
+            before:h-px
+            before:mr-4
+
+            after:grow
+            after:bg-indigo-500
+            after:h-px
+            after:ml-4
+          ">
+          가입한 모임
+        </section>
+        {joinedGroups.length === 0 ?
+          <section
+            className="min-h-[10vh] py-8 text-slate-400 text-sm text-center">
+              가입하신 모임이 없습니다.
+          </section>
+        :
+        null
+        }
+      </article>
+      <article>
+        <section className="pl-4 pt-1 pb-1 mb-4 bg-slate-200 text-slate-600 text-sm">
+          추천 모임
+        </section>
+        <section>
+          {isLoading ? 
+          skeletonCount.map((skeleton, index) => (
+            <li className="list-none mb-4" key={index}>
+              {skeleton}
+            </li>
+          ))
+          :
+          recommendGroupsCount.map((skeleton, index) => (
+            <li className="list-none" key={index}>
+              {skeleton}
+            </li>
+          ))}
+        </section>        
+      </article>
+      <PlusButton/>
+    </section>
+  );
 };
 
 export default MyGroup;
